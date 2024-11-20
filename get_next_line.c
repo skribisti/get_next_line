b42cfb276146d5c5927f621996595f97112fcbbd
Noe Rabinovici <norabino@c2r6p5.42perpignan.fr>
2024-11-20 11:55:13 +0100

    suite

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: norabino <norabino@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/19 09:38:08 by norabino          #+#    #+#             */
-/*   Updated: 2024/11/20 10:35:13 by norabino         ###   ########.fr       */
+/*   Updated: 2024/11/20 11:55:03 by norabino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,23 +45,58 @@ int	ft_size_line(char *buff)
 	return (i);
 }
 
-char	*ft_strndup(char *str, int n)
+char	*ft_substr(char *s, int start, int len)
 {
-	char	*tab;
 	int		i;
+	char	*str;
 
-	tab = (char *)malloc(n + 1);
-	if (!tab)
+	if (!s)
+		return (NULL);
+	if (start >= ft_strlen(s))
+		len = 0;
+	if (ft_strlen(s) - start < len)
+		len = ft_strlen(s) - start;
+	str = (char *)malloc(len + 1);
+	if (!str)
 		return (NULL);
 	i = 0;
-	while (str[i] && i < n)
+	while (i < len && s[i])
 	{
-		tab[i] = str[i];
+		str[i] = s[start + i];
 		i++;
 	}
-	tab[i] = 0;
-	return (tab);
+	str[i] = 0;
+	return (str);
 }
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	char	*str;
+	size_t	i;
+
+	str = malloc(nmemb * size);
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (i < nmemb * size)
+	{
+		str[i] = 0;
+		i++;
+	}
+	return (str);
+}
+/*char	*ft_read_file(int fd, char *res)
+{
+	char	*buffer;
+	int		bytes_read;
+
+	if (!res)
+		res = (char *)ft_calloc(1, sizeof(char));
+	buffer = (char *)calloc(BUFFER_SIZE + 1, sizeof(char));
+	bytes_read = 1;
+	while
+	
+}*/
 
 char	*get_next_line(int fd)
 {
@@ -72,13 +107,14 @@ char	*get_next_line(int fd)
 	static char		*remainder = "";
 	int				size_remainder;
 
-	if (fd <= 0)
-		return (printf("ERREUR OUVERTURE"), NULL);
-	buffer = (char *)malloc(BUFFER_SIZE + 1 * sizeof(char));
-	if (ft_strlen(remainder))
+	if (fd < 0)
 	{
-		
+		free(remainder);
+		buffer = NULL;
+		return (NULL);
 	}
+	//buffer = ft_read_file(fd, buffer);
+	
 	bytes_read = read(fd, buffer, BUFFER_SIZE);
 	if (bytes_read == -1) 
         return(printf("ERREUR LECTURE"), NULL);
@@ -88,15 +124,14 @@ char	*get_next_line(int fd)
 	if (bytes_read > size_l)
 	{
 		size_remainder = bytes_read - size_l;
-		remainder = ft_strndup(buffer + size_l, size_remainder);
+		remainder = ft_substr(buffer, size_l + 1, size_remainder);
 		if (size_remainder - 1)
-			printf("remainder = %s\n", remainder);
-		dup = ft_strndup(buffer, bytes_read - size_remainder +1);
+			printf("remainder = %s\n\n", remainder);
+		dup = ft_substr(buffer, 0, bytes_read - size_remainder + 1);
 		return (printf("Bytes read : %d\n", bytes_read - size_remainder), dup);
 	}
 	return (printf("Bytes read : %d\n", bytes_read), buffer);
 }
-
 
 #include <fcntl.h>
 int main() 
