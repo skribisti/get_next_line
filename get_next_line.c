1b0c45d2c262ae828267ae73e7c852f13d11b586
Noe Rabinovici <norabino@c3r2p6.42perpignan.fr>
2024-11-25 15:07:44 +0100

    leaks a gerer

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: norabino <norabino@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/19 09:38:08 by norabino          #+#    #+#             */
-/*   Updated: 2024/11/22 15:55:18 by norabino         ###   ########.fr       */
+/*   Updated: 2024/11/25 15:07:09 by norabino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -94,32 +94,22 @@ char	*get_next_line(int fd)
 }
 
 /*#include <fcntl.h>
+#include <stdio.h>
 int	main(void)
 {
 	char	*line;
 	int		i;
 	int		fd1;
-	int		fd2;
-	int		fd3;
-	fd1 = open("tests/test.txt", O_RDONLY);
-	fd2 = open("tests/test2.txt", O_RDONLY);
-	fd3 = open("tests/test3.txt", O_RDONLY);
-	i = 1;
-	while (i < 7)
+	
+	fd1 = open("test.txt", O_RDONLY);
+
+	i = 0;
+	printf("%s",get_next_line(fd1));
+	while (i < 6)
 	{
 		line = get_next_line(fd1);
-		//printf("line [%02d]: %s", i, line);
-		free(line);
-		line = get_next_line(fd2);
-		//printf("line [%02d]: %s", i, line);
-		free(line);
-		line = get_next_line(fd3);
-		//printf("line [%02d]: %s", i, line);
+		printf("line [%d]: %s", i, line);
 		free(line);
 		i++;
 	}
-	close(fd1);
-	close(fd2);
-	close(fd3);
-	return (0);
 }*/
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: norabino <norabino@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/19 09:39:49 by norabino          #+#    #+#             */
-/*   Updated: 2024/11/21 10:40:04 by norabino         ###   ########.fr       */
+/*   Updated: 2024/11/25 15:06:49 by norabino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,12 +21,13 @@
 # endif
 
 char	*get_next_line(int fd);
-char	*ft_get_line(char *str);
+char	*ft_get_a_line(char *str);
 char	*ft_read_to_remainder(int fd, char *remainder);
 
 int		ft_strlen(char *str);
 char	*ft_strchr(char *str, char c);
 char	*ft_substr(char *s, int start, int len);
 char	*ft_strjoin(char *s1, char *s2);
+void	*ft_check_join(char **s1, char **s2);
 
 #endif
